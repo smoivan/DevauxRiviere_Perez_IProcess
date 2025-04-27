eec9532f774ed5c7146eb5d169da1cd9aec0562d
smoivan <ivan.perez@efrei.net>
2025-04-27 16:42:00 +0200

    truc

--- bmp24.c
+++ bmp24.c
@@ -133,3 +133,130 @@ void bmp24_writePixelData(t_bmp24 *image, FILE *file) {
         }
     }
 }
+t_bmp24 *bmp24_loadImage(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("Erreur: Impossible d'ouvrir le fichier %s\n", filename);
+        return NULL;
+    }
+
+
+    uint16_t type;
+    int32_t width, height;
+    uint16_t bits;
+
+    file_rawRead(BITMAP_MAGIC, &type, sizeof(uint16_t), 1, file);
+    file_rawRead(BITMAP_WIDTH, &width, sizeof(int32_t), 1, file);
+    file_rawRead(BITMAP_HEIGHT, &height, sizeof(int32_t), 1, file);
+    file_rawRead(BITMAP_DEPTH, &bits, sizeof(uint16_t), 1, file);
+
+    if (type != BMP_TYPE) {
+        printf("Erreur: Le fichier n'est pas au format BMP valide\n");
+        fclose(file);
+        return NULL;
+    }
+
+    if (bits != DEFAULT_DEPTH) {
+        printf("Erreur: L'image n'est pas en 24 bits (profondeur: %d)\n", bits);
+        fclose(file);
+        return NULL;
+    }
+
+
+    t_bmp24 *img = bmp24_allocate(width, height, bits);
+    if (img == NULL) {
+        fclose(file);
+        return NULL;
+    }
+
+
+    file_rawRead(BITMAP_MAGIC, &img->header, sizeof(t_bmp_header), 1, file);
+    file_rawRead(HEADER_SIZE, &img->header_info, sizeof(t_bmp_info), 1, file);
+
+
+    bmp24_readPixelData(img, file);
+
+    fclose(file);
+    return img;
+}
+
+
+void bmp24_saveImage(t_bmp24 *img, const char *filename) {
+    if (img == NULL) {
+        printf("Erreur: Image invalide\n");
+        return;
+    }
+
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("Erreur: Impossible de créer le fichier %s\n", filename);
+        return;
+    }
+
+
+    file_rawWrite(BITMAP_MAGIC, &img->header, sizeof(t_bmp_header), 1, file);
+    file_rawWrite(HEADER_SIZE, &img->header_info, sizeof(t_bmp_info), 1, file);
+
+
+    bmp24_writePixelData(img, file);
+
+    fclose(file);
+}
+
+
+void bmp24_negative(t_bmp24 *img) {
+    if (img == NULL || img->data == NULL) {
+        printf("Erreur: Image invalide\n");
+        return;
+    }
+
+    for (int y = 0; y < img->height; y++) {
+        for (int x = 0; x < img->width; x++) {
+            img->data[y][x].red = 255 - img->data[y][x].red;
+            img->data[y][x].green = 255 - img->data[y][x].green;
+            img->data[y][x].blue = 255 - img->data[y][x].blue;
+        }
+    }
+}
+
+
+void bmp24_grayscale(t_bmp24 *img) {
+    if (img == NULL || img->data == NULL) {
+        printf("Erreur: Image invalide\n");
+        return;
+    }
+
+    for (int y = 0; y < img->height; y++) {
+        for (int x = 0; x < img->width; x++) {
+
+            uint8_t gray_value = (img->data[y][x].red + img->data[y][x].green + img->data[y][x].blue) / 3;
+
+
+            img->data[y][x].red = gray_value;
+            img->data[y][x].green = gray_value;
+            img->data[y][x].blue = gray_value;
+        }
+    }
+}
+
+
+void bmp24_brightness(t_bmp24 *img, int value) {
+    if (img == NULL || img->data == NULL) {
+        printf("Erreur: Image invalide\n");
+        return;
+    }
+
+    for (int y = 0; y < img->height; y++) {
+        for (int x = 0; x < img->width; x++) {
+
+            int new_red = img->data[y][x].red + value;
+            int new_green = img->data[y][x].green + value;
+            int new_blue = img->data[y][x].blue + value;
+
+
+            img->data[y][x].red = (new_red > 255) ? 255 : ((new_red < 0) ? 0 : new_red);
+            img->data[y][x].green = (new_green > 255) ? 255 : ((new_green < 0) ? 0 : new_green);
+            img->data[y][x].blue = (new_blue > 255) ? 255 : ((new_blue < 0) ? 0 : new_blue);
+        }
+    }
+}
