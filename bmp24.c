3b32fc426b7e44133e33ba5d12f45d54617e703b
smoivan <ivan.perez@efrei.net>
2025-04-27 16:37:19 +0200

    truc

--- bmp24.c
+++ bmp24.c
@@ -14,3 +14,25 @@ void file_rawWrite(uint32_t position, void *buffer, uint32_t size, size_t n, FIL
     fseek(file, position, SEEK_SET);
     fwrite(buffer, size, n, file);
 }
+
+t_pixel **bmp24_allocateDataPixels(int width, int height) {
+    t_pixel **pixels = (t_pixel **)malloc(height * sizeof(t_pixel *));
+    if (pixels == NULL) {
+        printf("Erreur: Impossible d'allouer de la mémoire pour les pixels\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < height; i++) {
+        pixels[i] = (t_pixel *)malloc(width * sizeof(t_pixel));
+        if (pixels[i] == NULL) {
+            printf("Erreur: Impossible d'allouer de la mémoire pour les pixels de la ligne %d\n", i);
+            for (int j = 0; j < i; j++) {
+                free(pixels[j]);
+            }
+            free(pixels);
+            return NULL;
+        }
+    }
+
+    return pixels;
+}
