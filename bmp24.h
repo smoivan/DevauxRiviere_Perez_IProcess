0aa2ce832806baeff69f823faf27ae82805afe08
smoivan <ivan.perez@efrei.net>
2025-04-27 16:29:04 +0200

    truc

--- bmp24.h
+++ bmp24.h
@@ -1,8 +1,17 @@
-//
-// Created by perez on 27/04/2025.
-//
+#ifndef BMP24_H
+#define BMP24_H
 
-#ifndef PROJET_C_BMP24_H
-#define PROJET_C_BMP24_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#endif //PROJET_C_BMP24_H
+
+#define BITMAP_MAGIC 0x00
+#define BITMAP_SIZE 0x02
+#define BITMAP_OFFSET 0x0A
+#define BITMAP_WIDTH 0x12
+#define BITMAP_HEIGHT 0x16
+#define BITMAP_DEPTH 0x1C
+#define BITMAP_SIZE_RAW 0x22
+
+
