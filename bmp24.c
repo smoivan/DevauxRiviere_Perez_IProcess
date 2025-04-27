3f30d9d6f470006bb189198208847e8489f9d29a
smoivan <ivan.perez@efrei.net>
2025-04-27 16:36:50 +0200

    truc

--- bmp24.c
+++ bmp24.c
@@ -1,3 +1,16 @@
-//
-// Created by perez on 27/04/2025.
-//
+#include "bmp24.h"
+#include <string.h>
+
+
+ */
+void file_rawRead(uint32_t position, void *buffer, uint32_t size, size_t n, FILE *file) {
+    fseek(file, position, SEEK_SET);
+    fread(buffer, size, n, file);
+}
+
+
+ */
+void file_rawWrite(uint32_t position, void *buffer, uint32_t size, size_t n, FILE *file) {
+    fseek(file, position, SEEK_SET);
+    fwrite(buffer, size, n, file);
+}
