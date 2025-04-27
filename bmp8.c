65562fdba3e59e0049bb7299d9127fcb913b52b3
Aurel1dr <aurelien.devaux--riviere@efrei.net>
2025-04-27 15:53:19 +0200

    changement 2

--- bmp8.c
+++ bmp8.c
@@ -1,26 +1,14 @@
-typedef struct {
-	unsigned char header[54];
-	unsigned char colorTable[1024];
-	unsigned char * data;
+#include <stdlib.h>
+#include <string.h>
+#include "bmp8.h"
 
-	unsigned int width;
-	unsigned int height;
-	unsigned int colorDepth;
-	unsigned int dataSize;
-} t_bmp8;
+t_bmp8 *bmp8_loadImage(const char *filename) {
+    FILE *image = fopen(filename, "rb");
 
 
-	t_bmp8 * bmp8_loadImage(const char * filename);
+    if (image == NULL) {
+        printf("Erreur lors de l'ouverture du fichier, le fichier n'existe pas!\n");
+        return NULL;
+    }
 
-	void bmp8_saveImage(const char * filename, t_bmp8 * img);
-
-	void bmp8_free(t_bmp8 * img);
-
-	void bmp8_printInfo(t_bmp8 * img);
-
-	void bmp8_negative(t_bmp8 * img);
-
-	void bmp8_brightness(t_bmp8 * img, int value);
-
-	void bmp8_threshold(t_bmp8 * img, int threshold);
 
--- tmp8.h
+++ tmp8.h
@@ -1,8 +1,21 @@
-//
-// Created by perez on 20/03/2025.
-//
+typedef struct {
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
 
-#ifndef TMP8_H
-#define TMP8_H
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+} t_bmp8;
 
-#endif //TMP8_H
+
+    t_bmp8 * bmp8_loadImage(const char * filename);
+
+    void bmp8_saveImage(const char * filename, t_bmp8 * img);
+    void bmp8_free(t_bmp8 * img);
+    void bmp8_printInfo(t_bmp8 * img);
+    void bmp8_negative(t_bmp8 * img);
+    void bmp8_brightness(t_bmp8 * img, int value);
+    void bmp8_threshold(t_bmp8 * img, int threshold);
+
