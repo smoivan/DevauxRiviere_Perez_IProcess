2f29b1449992fc05626895028db7d599d65ef57d
Aurel1dr <aurelien.devaux--riviere@efrei.net>
2025-05-25 14:32:15 +0200

    résolution probleme

--- bmp8.h
+++ bmp8.h
@@ -12,7 +12,7 @@ typedef struct {
 
     t_bmp8 * bmp8_loadImage(const char * filename);
 
-    void bmp8_saveImage(const char * filename, t_bmp8 * img);
+    int bmp8_saveImage(const char * filename, t_bmp8 * img);
     void bmp8_free(t_bmp8 * img);
     void bmp8_printInfo(t_bmp8 * img);
     void bmp8_negative(t_bmp8 * img);
